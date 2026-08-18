2c5c316f3d53123e589d109adf73f0191bde827a
marco201091-glitch <marco201091@gmail.com>
2026-08-18 11:58:06 +0200

    chore: prepare release 8.1.0

--- lib/legal-site.ts
+++ lib/legal-site.ts
@@ -1,7 +1,7 @@
 export const LEGAL_SITE_NAME = 'MTG Life Counter & Analytics: Commander';
 export const LEGAL_BRAND_NAME = 'blackistoostrong';
 export const LEGAL_CONTROLLER_NAME = 'Marco Andreani';
-export const APP_VERSION = '8.0.3';
+export const APP_VERSION = '8.1.0';
 export const LEGAL_LAST_UPDATED = '2026-07-29';
 export const OFFICIAL_SUPPORT_EMAIL = 'support@phyrexianarena.dpdns.org';
 export const FAN_CONTENT_NOTICE = `${LEGAL_SITE_NAME} is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards. Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC.`;
